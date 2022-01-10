0344fe7ae87a01ff8010d9078fa19bca6d81028b
Project-demo <anhtuan0512>
2022-01-11 00:31:04 +0700

    push source code

---
+++ products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    id: 0,
+    name: "Plant-Based Burger",
+    price: 90.000,
+    instock: 100,
+    description:
+      "Protein for every day of the week.",
+    imgSrc: "https://beyond-meat-cms-production.s3.us-west-2.amazonaws.com/1768da07-a2b1-44bb-8a56-a0c2f95be780.png",
+  },
+  {
+    id: 1,
+    name: "Plant-Based MEATBALL ",
+    price: 90.000,
+    instock: 43,
+    description:
+      "Protein for every day of the week.",
+    imgSrc: "https://beyond-meat-cms-production.s3.us-west-2.amazonaws.com/8fe81b3b-73a6-4857-96de-80fa944567e1.png",
+  },
+  {
+    id: 2,
+    name: "Plant-Based BEEF ",
+    price: 89.000,
+    instock: 10,
+    description:
+      "Protein for every day of the week.",
+    imgSrc: "https://beyond-meat-cms-production.s3.us-west-2.amazonaws.com/f720e3e5-b61e-43b1-85fa-616bc3424d42.png",
+  },
+  {
+    id: 3,
+    name: "Plant-Based CHICKEN ",
+    price: 88.000,
+    instock: 5,
+    description:
+      "Protein for every day of the week.",
+    imgSrc: "https://beyond-meat-cms-production.s3.us-west-2.amazonaws.com/e8d65b81-04e0-49e5-a7e4-511a41ad2b54.png",
+  },
+  {
+    id: 4,
+    name: "TRIAL BUNDLE",
+    price: 140.000,
+    instock: 4,
+    description:
+      "A mix of all our faves from breakfast to dinner.",
+    imgSrc: "https://beyond-meat-cms-production.s3.us-west-2.amazonaws.com/36af9701-ec5d-4057-a5f6-e20184dc61bd.png",
+  },
+  {
+    id: 5,
+    name: "Plant-Based SAUSAGE ",
+    price: 91.000,
+    instock: 40,
+    description:
+      "Protein for every day of the week.",
+    imgSrc: "https://beyond-meat-cms-production.s3.us-west-2.amazonaws.com/e8fdfd77-297d-4257-b166-d38a0593d8e0.png",
+  },
+];
