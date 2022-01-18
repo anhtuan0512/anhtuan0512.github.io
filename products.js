3144365afaa7104532737cf1c3f37f79c5109e4e
Project-demo <anhtuan0512>
2022-01-18 09:50:41 +0700

    push source code

--- products.js
+++ products.js
@@ -1,39 +1,39 @@
 const products = [
   {
     id: 0,
-    name: "Plant-Based Burger",
+    name: " Burger VGMEAT",
     price: 90.000,
     instock: 100,
     description:
       "Protein for every day of the week.",
-    imgSrc: "https://beyond-meat-cms-production.s3.us-west-2.amazonaws.com/1768da07-a2b1-44bb-8a56-a0c2f95be780.png",
+    imgSrc: "burger.jpg",
   },
   {
     id: 1,
-    name: "Plant-Based MEATBALL ",
+    name: " MEATBALL VGMEAT ",
     price: 90.000,
     instock: 43,
     description:
       "Protein for every day of the week.",
-    imgSrc: "https://beyond-meat-cms-production.s3.us-west-2.amazonaws.com/8fe81b3b-73a6-4857-96de-80fa944567e1.png",
+    imgSrc: "image1.png",
   },
   {
     id: 2,
-    name: "Plant-Based BEEF ",
+    name: " BEEF VGMEAT ",
     price: 89.000,
     instock: 10,
     description:
       "Protein for every day of the week.",
-    imgSrc: "https://beyond-meat-cms-production.s3.us-west-2.amazonaws.com/f720e3e5-b61e-43b1-85fa-616bc3424d42.png",
+    imgSrc: "image.png",
   },
   {
     id: 3,
-    name: "Plant-Based CHICKEN ",
+    name: " CHICKEN VGMEAT ",
     price: 88.000,
     instock: 5,
     description:
       "Protein for every day of the week.",
-    imgSrc: "https://beyond-meat-cms-production.s3.us-west-2.amazonaws.com/e8d65b81-04e0-49e5-a7e4-511a41ad2b54.png",
+    imgSrc: "chicken.jpg",
   },
   {
     id: 4,
@@ -42,15 +42,15 @@ const products = [
     instock: 4,
     description:
       "A mix of all our faves from breakfast to dinner.",
-    imgSrc: "https://beyond-meat-cms-production.s3.us-west-2.amazonaws.com/36af9701-ec5d-4057-a5f6-e20184dc61bd.png",
+    imgSrc: "all-in-one.jpg",
   },
   {
     id: 5,
-    name: "Plant-Based SAUSAGE ",
+    name: " SAUSAGE VGMEAT ",
     price: 91.000,
     instock: 40,
     description:
       "Protein for every day of the week.",
-    imgSrc: "https://beyond-meat-cms-production.s3.us-west-2.amazonaws.com/e8fdfd77-297d-4257-b166-d38a0593d8e0.png",
+    imgSrc: "sausage.jpg",
   },
 ];
